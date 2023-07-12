fd90102d1b38f05082f5ff1aec3e8e4d67c8b8b3
bram <brammahendrawan693@gmail.com>
2023-07-13 01:21:50 +0700

    tipe data karakter
    Signed-off-by: bram <brammahendrawan693@gmail.com>

---
+++ 03-tipe-data-karakter.kt
@@ -0,0 +1,5 @@
+fun main() { // tipe data karakter menggunakan petik satu dan hanya bisa satu karakter saja
+    var karakter: Char = 'A'
+    var karakter2: Char = '1'
+    println("Char: " + karakter + " " + karakter2)
+}
